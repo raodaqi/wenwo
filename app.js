51ea56ed1f8293d27588be35a46d9e744a26caf3
raodaqi <raodaqi@qq.com>
2016-04-13 19:27:22 +0800

    create

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var todos = require('./routes/todos');
 var cloud = require('./cloud');
+var more = require('./routes/more');
 
 var app = express();
 
@@ -59,6 +60,7 @@ app.post('/test', function(req, res) {
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
+app.use('/more', more);
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
---
+++ routes/more.js
@@ -0,0 +1,28 @@
+var router = require('express').Router();
+var AV = require('leanengine');
+
+// `AV.Object.extend` 方法一定要放在全局变量，否则会造成堆栈溢出。
+// 详见： https://leancloud.cn/docs/js_guide.html#对象
+// var Todo = AV.Object.extend('Todo');
+
+// 查询 Todo 列表
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+// 新增 Todo 项目
+router.post('/', function(req, res, next) {
+  var content = req.body.content;
+  var todo = new Todo();
+  todo.set('content', content);
+  todo.save(null, {
+    success: function(todo) {
+      res.redirect('/todos');
+    },
+    error: function(err) {
+      next(err);
+    }
+  })
+})
+
+module.exports = router;
