b3c17be4e4e1d42a9ebf6f03fd270e6a0eae74f2
HanQi <1464467970@qq.com>
2016-04-15 15:38:21 +0800

    create

--- routes/user.js
+++ routes/user.js
@@ -9,19 +9,10 @@ var AV = require('leanengine');
 var Post = AV.Object.extend('UserInfo');
 
 // 查询 Todo 列表
-router.get('/', function(req, res, next) {
+router.get('/regist', function(req, res, next) {
     var post = new Post();
 
-    post.save({
-        tagName: '每个 JavaScript 程序员必备的 8 个开发工具',
-        tagUrl: "123"
-    }).then(function(post) {
-        // 实例已经成功保存.
-        console.log('suc');
-    }, function(err) {
-        // 失败了.
-    });
-    res.send('user');
+    console.log(req);
 });
 
 // // 新增 Todo 项目
