58e1d1029ce06dba3b0ce571a9a9e9096b277dd4
HanQi <1464467970@qq.com>
2016-05-09 23:16:41 +0800

    authorization test

--- routes/authorization.js
+++ routes/authorization.js
@@ -8,4 +8,42 @@ var AV = require('leanengine');
 // 详见： https://leancloud.cn/docs/js_guide.html#对象
 
 
+router.get('/wx', function(req, res, next) {
+    var appid = 'wx99f15635dd7d9e3c';
+    var secret = '9157e84975386b6dee6a499cc639973e';
+    //var url = req.body.url;
+    var url = req.rawHeaders[1];
+    console.log(url);
+    getCode(appid, url, {
+        success: function (result) {
+            console.log(result);
+            //var code = result.data.code;
+
+        }
+    });
+})
+
+function getCode(appid, url, callback) {
+    var urlApi = url + "/";
+    urlApi = encodeURIComponent(urlApi);
+    console.log(urlApi);
+    AV.Cloud.httpRequest({
+        url: "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appid+"&redirect_uri="+urlApi+"&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect",
+        success: function(httpResponse) {
+            // console.log(httpResponse);
+            callback.success(httpResponse);
+        },
+        error: function(httpResponse) {
+            console.error('Request failed with response code ' + httpResponse.status);
+        }
+    });
+}
+
+function getAccessToken(code, callback) {
+    
+}
+
+function getUserInfo() {
+
+}
 module.exports = router;
