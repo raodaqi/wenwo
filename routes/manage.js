0bcafad5f878838481b9d95e76f6b36bf0c296fb
HanQi <1464467970@qq.com>
2016-05-25 00:04:16 +0800

    admin account

--- routes/manage.js
+++ routes/manage.js
@@ -4,9 +4,98 @@
 
 var router = require('express').Router();
 var AV = require('leanengine');
+var Admin = AV.Object.extend('AdminAccount');
 
 router.get('/', function(req, res, next) {
     res.render('manage/index');
+});
+
+router.post('/regist', function(req, res, next) {
+    var adminName = req.query.name;
+    var password = req.query.password;
+
+    if (adminName == '' || adminName == null) {
+        var result = {
+            code : 400,
+            message : '缺少参数name'
+        }
+        res.send(result);
+        return;
+    }
+    if (password == '' || password == null) {
+        var result = {
+            code : 400,
+            message : '缺少参数password'
+        }
+        res.send(result);
+        return;
+    }
+    var query = new AV.Query('AdminAccount');
+    query.find().then(function (resultes) {
+        for (var i = 0; i < resultes.length; i++) {
+            if (resultes[i].get('adminName') == adminName) {
+                var result = {
+                    code : 600,
+                    message : '该用户名已经被注册'
+                }
+                res.send(result);
+                return;
+            }
+        }
+        var account = new Admin();
+        account.set('adminName', adminName);
+        account.set('password', password);
+        account.save().then(function (account) {
+            var result = {
+                code : 200,
+                message : '操作成功'
+            }
+            res.send(result);
+            return;
+        });
+    });
+
+});
+
+router.post('/login', function (req, res, next) {
+    var name = req.query.name;
+    var password = req.query.password;
+    if (name == '' || name == null) {
+        var result = {
+            code : 400,
+            message : '缺少参数name'
+        }
+        res.send(result);
+        return;
+    }
+    if (password == '' || password == null) {
+        var result = {
+            code : 400,
+            message : '缺少参数password'
+        }
+        res.send(result);
+        return;
+    }
+    var query = new AV.Query('AdminAccount');
+    query.find().then(function (resultes) {
+        for (var i = 0; i < resultes.length; i++ ) {
+            if (resultes[i].get('adminName') == name && resultes[i].get('password') == password) {
+                var result = {
+                    code : 200,
+                    user : resultes[i].get('adminName'),
+                    message : '登录成功'
+                }
+                res.send(result);
+                return;
+            }
+        }
+        var result = {
+            code : 400,
+            message : '用户名或密码错误'
+        }
+        res.send(result);
+        return;
+    });
 });
 
 router.get('/getviews', function(req, res, next) {
