fc676871b5641e87519880925aa074444eb6b586
raodaqi <raodaqi@qq.com>
2016-05-24 18:39:38 +0800

    发布二级标签个数限制逻辑编写

--- public/js/common.js
+++ public/js/common.js
@@ -76,3 +76,15 @@ function getAddress(lnglatXY,callback){
             }
         }); 
     }
+
+function GetLength(str) {
+  ///<summary>获得字符串实际长度，中文2，英文1</summary>
+  ///<param name="str">要获得长度的字符串</param>
+  var realLength = 0, len = str.length, charCode = -1;
+  for (var i = 0; i < len; i++) {
+    charCode = str.charCodeAt(i);
+    if (charCode >= 0 && charCode <= 128) realLength += 1;
+    else realLength += 2;
+  }
+  return realLength;
+};
