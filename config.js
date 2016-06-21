b655476b5842c73b532b7d52bbed0a6161e4e511
HanQi <1464467970@qq.com>
2016-06-21 22:01:16 +0800

    测试版

--- config.js
+++ config.js
@@ -3,11 +3,11 @@
  */
 module.exports = {
     //正式版
-    "appid" : "wx99f15635dd7d9e3c",
-    "appsecret" : "9157e84975386b6dee6a499cc639973e"
+    // "appid" : "wx99f15635dd7d9e3c",
+    // "appsecret" : "9157e84975386b6dee6a499cc639973e"
 
 
     //测试版
-    // "appid" : "wxb0f5d00833204d63",
-    // "appsecret" : "44ab2dc42222c6f619a12dd8c5dc63d9"
+    "appid" : "wxb0f5d00833204d63",
+    "appsecret" : "44ab2dc42222c6f619a12dd8c5dc63d9"
 }
