8f2d4e6729a12c01bb749bf527a80dfe9e3375e6
HanQi <1464467970@qq.com>
2016-07-20 17:45:57 +0800

    test2

--- routes/authorization.js
+++ routes/authorization.js
@@ -265,7 +265,7 @@ router.get('/pay', function(req, res, next) {
                         detail: '来自'+ask.get('createByName')+'的美食推荐',
                         out_trade_no: '20160331'+Math.random().toString().substr(2, 10),
                         total_fee: totalFee,
-                        attach:attach,
+                        // attach:attach,
                         spbill_create_ip: ip,
                         notify_url:notifyUrl
                     }, function(err, result){
