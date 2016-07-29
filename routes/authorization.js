ef0c6bcca06e8af16a142729d1e8269d6189d9b3
HanQi <1464467970@qq.com>
2016-07-29 12:43:30 +0800

    test

--- routes/authorization.js
+++ routes/authorization.js
@@ -600,69 +600,82 @@ router.get('/', function(req, res, next) {
                         userhead += '/';
                     }
 
-                    console.log(username);
-                    console.log(userhead);
-                    console.log(openid);
-                    AV.User._logInWith('weixin', {
-                        'authData': {
-                            "openid": openid,
-                            "access_token": accessToken,
-                            "expires_in": expiresIn
-                        }
-                    }).then(function(user) {
-                        //返回绑定后的用户
-                        console.log('登录成功');
-                        //console.log(user.get('user'));
+
 
                         isFocus(openid, accessToken, {
 
                             success:function (focusResult) {
+                                console.log(focusResult.data);
 
-                                console.log(focusResult);
+                                console.log(username);
+                                console.log(userhead);
+                                console.log(openid);
+                                AV.User._logInWith('weixin', {
+                                    'authData': {
+                                        "openid": openid,
+                                        "access_token": accessToken,
+                                        "expires_in": expiresIn
+                                    }
+                                }).then(function(user) {
+                                    //返回绑定后的用户
+                                    console.log('登录成功');
+                                    //console.log(user.get('user'));
 
-                                if (user.get('user') != null) {
-                                    // console.log('haved');
-                                    // var user = AV.User.current();
-                                    // console.log(user);
-                                    //var url = 'http://www.wenwobei.com/?username='+user.get('user');
-                                    var url = urlReq + '?username='+user.get('user')+'&isfocus='+focusResult.subscribe;
-                                    resG.redirect(url);
-                                    return;
 
-                                }
-                                else {
-                                    // console.log('no');
-                                    var post = new Post();
-                                    var wallet = new Wallet();
-                                    wallet.set('money', 0);
-                                    wallet.save().then(function (wallet) {
-                                        // console.log(wallet);
-                                        //post.set('userName', post.id);
-                                        post.set('uName', username);
-                                        post.set('userHead', userhead);
-                                        post.set('wallet', wallet);
-                                        post.save().then(function (post) {
-                                            post.set('userName', post.id);
+                                    if (user.get('user') != null) {
+                                        // console.log('haved');
+                                        // var user = AV.User.current();
+                                        // console.log(user);
+                                        //var url = 'http://www.wenwobei.com/?username='+user.get('user');
+                                        var url = urlReq + '?username='+user.get('user')+'&isfocus='+focusResult.subscribe;
+                                        resG.redirect(url);
+                                        return;
+
+                                    }
+                                    else {
+                                        // console.log('no');
+                                        var post = new Post();
+                                        var wallet = new Wallet();
+                                        wallet.set('money', 0);
+                                        wallet.save().then(function (wallet) {
+                                            // console.log(wallet);
+                                            //post.set('userName', post.id);
+                                            post.set('uName', username);
+                                            post.set('userHead', userhead);
+                                            post.set('wallet', wallet);
                                             post.save().then(function (post) {
-                                                user.set('userInfo', post);
-                                                user.set('user', post.id);
-                                                user.set('wallet', wallet);
-                                                user.save().then(function (user) {
-                                                    // var user = AV.User.current();
-                                                    // console.log(user);
-                                                    //var url = 'http://www.wenwobei.com/?username='+user.get('user');
-                                                    var url = urlReq + '?username='+user.get('user')+'&isfocus='+focusResult.subscribe;
-                                                    resG.redirect(url);
-                                                    return;
+                                                post.set('userName', post.id);
+                                                post.save().then(function (post) {
+                                                    user.set('userInfo', post);
+                                                    user.set('user', post.id);
+                                                    user.set('wallet', wallet);
+                                                    user.save().then(function (user) {
+                                                        // var user = AV.User.current();
+                                                        // console.log(user);
+                                                        //var url = 'http://www.wenwobei.com/?username='+user.get('user');
+                                                        var url = urlReq + '?username='+user.get('user')+'&isfocus='+focusResult.subscribe;
+                                                        resG.redirect(url);
+                                                        return;
 
 
+                                                    });
                                                 });
+
                                             });
-
                                         });
-                                    });
 
-                                }
+                                    }
+
+
+                                }, function(error) {
+                                    // console.log(error);
+                                });
+
+
+
+
+
+
 
 
                             }
@@ -673,9 +686,7 @@ router.get('/', function(req, res, next) {
 
 
 
-                    }, function(error) {
-                        // console.log(error);
-                    });
+
 
                     // var data = {
                     //     username:username,
