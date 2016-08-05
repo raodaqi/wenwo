7201267967a1f139d86cae6d15dfe122cbf96f85
HanQi <1464467970@qq.com>
2016-08-05 12:03:02 +0800

    0元信息购买失败

--- routes/authorization.js
+++ routes/authorization.js
@@ -290,7 +290,8 @@ router.get('/pay', function(req, res, next) {
 
                 var score = parseInt(ask.get('score'));
 
-                if (score < 10) {
+
+                if (score < 10 || ask.get('askPrice') == 0) {
 
                     var query = new AV.Query('UserInfo');
                     query.get(userName).then(function (user) {
